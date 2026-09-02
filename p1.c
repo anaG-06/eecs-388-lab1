c3d1bfd78e4fd473ddebfc342c033eead1269223
Ana <anavirgi2006@gmail.com>
2026-09-02 16:02:54 -0500

    add p1

---
+++ p1.c
@@ -0,0 +1,93 @@
+/*
+Author: Ana Gonzalez Yuil
+Date Modified: 9.2.26
+Lab 1, Problem 1
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int first_op; //the first operand from the user
+    int second_op; // the second operand from the user; not used if squaring or cubing
+    char operator;
+    int result;
+
+    printf("Enter operation:\n");
+    scanf("%c", &operator);
+
+    // printf("%c\n",operator);
+
+    if (operator == '+')// adds operands
+    {
+        printf("Enter 1st operand:\n");
+        scanf("%d",&first_op);
+
+        printf("Enter 2nd operand:\n");
+        scanf("%d",&second_op);
+
+        result = first_op + second_op;
+
+        printf("%d\n",result);
+    }
+
+    else if (operator == '-')// subtracts operands
+    {
+        printf("Enter 1st operand:\n");
+        scanf("%d",&first_op);
+
+        printf("Enter 2nd operand:\n");
+        scanf("%d",&second_op);
+
+        result = first_op - second_op;
+
+        printf("%d\n",result);
+    }
+
+    else if (operator == '*')// mutiplies operands
+    {
+        printf("Enter 1st operand:\n");
+        scanf("%d",&first_op);
+
+        printf("Enter 2nd operand:\n");
+        scanf("%d",&second_op);
+
+        result = first_op * second_op;
+
+        printf("%d\n",result);
+    }
+
+    else if (operator == '/') //divides operands, result is floored.
+    {
+        printf("Enter 1st operand:\n");
+        scanf("%d",&first_op);
+
+        printf("Enter 2nd operand:\n");
+        scanf("%d",&second_op);
+
+        result = first_op / second_op;
+
+        printf("%d\n",result);
+    }
+
+    else if (operator == 's') //divides operands, result is floored.
+    {
+        printf("Enter 1st operand:\n");
+        scanf("%d",&first_op);
+
+        result = first_op * first_op;
+
+        printf("%d\n",result);
+    }
+
+    else if (operator == 'c') //divides operands, result is floored.
+    {
+        printf("Enter 1st operand:\n");
+        scanf("%d",&first_op);
+
+        result = first_op * first_op * first_op;
+
+        printf("%d\n",result);
+    }
+}
