e22a2d15b7eddf6953078b167fcc5e5709a02e8f
Ana <anavirgi2006@gmail.com>
2026-09-04 16:35:42 -0500

    rename p4.c to p5.c

---
+++ p5.c
@@ -0,0 +1,37 @@
+/*
+Author: Ana Gonzalez Yuil
+Date Modified: 9.2.26
+Lab 1, Problem 5
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int len; //stores length of array defined by user input
+    int number; //stores element that will be added to array
+    int count = 0; // stores count of consecutively increasing ints
+
+    printf("How big is the array?\n");
+    scanf("%d", &len);
+
+    int arr[len]; //initialize array with user defined size
+
+    printf("Enter %d integers:\n", len);
+    
+    //adds values to arr using for loop to ensure correct amount of elements with len
+    for (int i=0; i < len; i++)
+    {
+        scanf("%d",&number);
+        arr[i] = number;
+
+        if (arr[i] > arr[i-1]  && i != 0) 
+        //if the current value added is greater than the previous value, increment count by 1
+        //excludes the first element to prevent out of bounds
+        {
+            count++;
+        }
+    }
+
+    printf("%d\n", count);
+}
